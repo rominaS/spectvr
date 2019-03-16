4c9ff000c9a50bf3e92a25bb653bc057c0f7b5a3
rominaS <romina.saravi@icloud.com>
2019-03-15 22:39:59 -0400

    frontend

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,35 @@
-(function(){
-    "use strict";
-        
-    window.addEventListener('load', function(){
-        document.getElementById('sign_up_form').addEventListener('submit', function(e){
-            e.preventDefault();
-            let username = document.getElementById('username').value;
-            let password = document.getElementById('password').value;
-            document.getElementById('sign_up_form').reset();
-            backend.signUp(username, password, function(res, err){});
-        });
-    });
-    
-}());
+(function() {
+  "use strict";
 
+  api.onError(function(err) {
+    console.error("[error]", err);
+  });
+
+  api.onError(function(err) {
+    var error_box = document.querySelector("#error_box");
+    error_box.innerHTML = err;
+    error_box.style.visibility = "visible";
+  });
+
+  api.onUserUpdate(function(username) {
+    document.querySelector("#signin_button").style.visibility = username
+      ? "hidden"
+      : "visible";
+    document.querySelector("#signout_button").style.visibility = username
+      ? "visible"
+      : "hidden";
+    //document.querySelector('#create_message_form').style.visibility = (username)? 'visible' : 'hidden';
+  });
+
+  window.addEventListener("load", function() {
+    document
+      .getElementById("sign_up_form")
+      .addEventListener("submit", function(e) {
+        e.preventDefault();
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+        document.getElementById("sign_up_form").reset();
+        backend.signUp(username, password, function(res, err) {});
+      });
+  });
+})();
