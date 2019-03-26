25b8ecaa98f98e6df2c8d9b2591d01432db8b14a
charles.ruan@mail.utoronto.ca <yesilikechee@live.com>
2019-03-26 11:22:46 -0400

    S3 changes

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ aws.config.update({
     // Your SECRET ACCESS KEY from AWS should go here,
     // Never share it!
     // Setup Env Variable, e.g: process.env.SECRET_ACCESS_KEY
-    secretAccessKey: "ab7786ad6",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     // Not working key, Your ACCESS KEY ID from AWS should go here,
     // Never share it!
     // Setup Env Variable, e.g: process.env.ACCESS_KEY_ID
-    accessKeyId: "ab7786ad6",
-    region: 'us-east-1' // region of your bucket
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    region: 'us-east-2' // region of your bucket
 });
 
 const s3 = new aws.S3();
