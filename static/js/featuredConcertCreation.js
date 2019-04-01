0395b5f6adbbec02226c29b2558faab4ccfb36fe
lovehela <helaina.love@mail.utoronto.ca>
2019-03-31 21:15:31 -0400

    connected the index page

---
+++ static/js/featuredConcertCreation.js
@@ -0,0 +1,36 @@
+(function() {
+  "use strict";
+
+   function createConcertThumbnail(url, name, artistName, date, time, price){
+    return "    <section class='container content-section'><div class='shop-items'>"+
+        +"<div class='shop-item'>"
+         + "<span class='shop-item-title' name='concertName'"
+            + ">"+name+"</span"
+          + ">"
+          + "<img"
+            + "class='shop-item-image'"
+            + "alt='concert-img'"
+            + "src='" + url + "'"
+          + "/>"
+          + "<div class='shop-item-details'>"
+           + " <span class='' name='artistName'>"+artistName+" </span>"
+           + " <span class='' name='concertDate'>"+ date+"</span>"
+           + " <span class='' name='concertTime'>"+time+"</span>"
+           + " <span class='shop-item-price'>$"+price +"</span>"
+           + "<a href='/concertPage.html' id='buyConcert'>"
+           +  " Buy Ticket"
+           + "</a>"
+       + "   </div>"
+      + "  </div>"
+      + "</div> </section>"
+   }
+  window.addEventListener("load", function() {
+    backend.getVideos(0, 100, function(err, res) {
+        document.getElementById("featuredConcerts").innerHTML = "";
+        let concert;
+        for(concert in res){
+            document.getElementById("displayCart").innerHTML += createConcertThumbnail(concert.url, concert.title);
+        }
+    });
+  });
+})();
